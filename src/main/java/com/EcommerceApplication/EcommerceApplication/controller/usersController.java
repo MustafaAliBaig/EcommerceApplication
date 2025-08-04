55bd28e114eed117a146ccb603183bc3b6171d08
MustafaAliBaig <mirzabaig.mustafaali@gmail.com>
2025-08-04 11:22:53 -0700

    updated UserController With Below Following 1.Get All UserNames 2.Create User 3. Update User 4.Delete User

--- src/main/java/com/EcommerceApplication/EcommerceApplication/controller/usersController.java
+++ src/main/java/com/EcommerceApplication/EcommerceApplication/controller/usersController.java
@@ -6,10 +6,7 @@ import com.EcommerceApplication.EcommerceApplication.service.usersService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -24,19 +21,25 @@ public class usersController {
     //We should always create object of Service call in controller
 
 
+//    @GetMapping
+//    public ResponseEntity<usersModel> getAllUsers() {
+//       // List<usersModel> data = usersService.getAllUsers();
+//        return usersService.getAllUsers()
+//
+//    }
+
     @GetMapping
-    public ResponseEntity<?> getAllUsers() {
-        List<usersModel> data = usersService.getAllUsers();
-        return ResponseEntity.ok(data);
+    public List<usersModel> getAllUsers() {
+        return usersService.getAllUsers();
     }
 
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getUserById(@PathVariable Long id) {
+    public ResponseEntity<usersModel> getUserById(@PathVariable Long id) {
         usersModel data = usersService.getUserById(id);
         return ResponseEntity.ok(data);
     }
-    
+
     @GetMapping("/email/{email}")
     public ResponseEntity<usersModel> getUserByEmail(@PathVariable String email) {
         return usersService.getUserByEmail(email)
@@ -44,5 +47,27 @@ public class usersController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @GetMapping("/username/{username}")
+    public ResponseEntity<usersModel> getUserByUsername(@PathVariable String username) {
+        return usersService.getUserByUsername(username)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
 
+    @PostMapping("/create")
+    public ResponseEntity<usersModel> createUser(@RequestBody usersModel user) {
+        return ResponseEntity.ok(usersService.createUser(user));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<usersModel> updateUser(@PathVariable Long id, @RequestBody usersModel user) {
+        usersModel updatedUser = usersService.updateUser(id, user);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        usersService.deleteUser(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
